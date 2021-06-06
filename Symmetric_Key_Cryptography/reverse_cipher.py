a5201ecefa460ec8b2de43d58c9ca531deb2eb0b
Dunga Datta <dattadunga@gmail.com>
2021-06-07 01:19:07 +0530

    Added reverse cipher.

---
+++ Symmetric_Key_Cryptography/reverse_cipher.py
@@ -0,0 +1,15 @@
+def rev_string_cipher(text):
+    # function reverses the given string
+    size = len(text) - 1
+    encoded = ""
+    # code to reverse the string given
+    while size >= 0:
+        encoded = encoded + text[size]
+        size = size - 1
+
+    print("The cipher text is :", encoded)
+
+
+# takes input
+message = str(input("ENTER A CIPHER TEXT: "))
+rev_string_cipher(message)
