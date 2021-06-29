66bff1d32358feaca60c8eb45975877346c0373e
datta-agni <dattadunga@gmail.com>
2021-06-29 22:25:49 +0530

    Added Caesar Cipher with brute force decryption process.

--- Symmetric_Key_Cryptography/caesar_cipher.py
+++ Symmetric_Key_Cryptography/caesar_cipher.py
@@ -1,25 +1,77 @@
-# caesar cipher
-def encrypt(text, s):
+from string import ascii_letters
+from typing import Dict, Optional
+
+def encrypt(input_string: str, key: int, alphabet: Optional[str] = None) -> str:
+    # Set default alphabet to lower and upper case english chars
+    alpha = alphabet or ascii_letters
+
+    # The final result string
     result = ""
 
-    # traverse text
-    for i in range(len(text)):
-        char = text[i]
-
-        # Encrypt uppercase characters
-        if char.isupper():
-            result += chr((ord(char) + s - 65) % 26 + 65)
-
-        # Encrypt lowercase characters
+    for character in input_string:
+        if character not in alpha:
+            # Append without encryption if character is not in the alphabet
+            result += character
         else:
-            result += chr((ord(char) + s - 97) % 26 + 97)
+            # Get the index of the new key and make sure it isn't too large
+            new_key = (alpha.index(character) + key) % len(alpha)
+
+            # Append the encoded character to the alphabet
+            result += alpha[new_key]
 
     return result
 
 
-# check the above function
-message = str(input(" ENTER THE TEXT TO BE ENCODED WITH CAESER TECHNIQUE: "))
-shift = int(input(" ENTER THE SHIFT FOR CAESER TECHNIQUE:"))
-print("Text  : " + message)
-print("Shift : " + str(shift))
-print("Cipher: " + encrypt(message, shift))
+def decrypt(input_string: str, key: int, alphabet: Optional[str] = None) -> str:
+    # Turn on decode mode by making the key negative
+    key *= -1
+
+    return encrypt(input_string, key, alphabet)
+
+
+def brute_force(input_string: str, alphabet: Optional[str] = None) -> Dict[int, str]:
+    # Set default alphabet to lower and upper case english chars
+    alpha = alphabet or ascii_letters
+
+    # To store data on all the combinations
+    brute_force_data = {}
+
+    # Cycle through each combination
+    for key in range(1, len(alpha) + 1):
+        # Decrypt the message and store the result in the data
+        brute_force_data[key] = decrypt(input_string, key, alpha)
+
+    return brute_force_data
+
+
+if __name__ == "__main__":
+    while True:
+        print(f'\n{"-" * 10}\n Menu\n{"-" * 10}')
+        print(*["1.Encrypt", "2.Decrypt", "3.BruteForce", "4.Quit"], sep="\n")
+
+        # get user input
+        choice = input("\nWhat would you like to do?: ").strip() or "4"
+
+        # run functions based on what the user chose
+        if choice not in ("1", "2", "3", "4"):
+            print("Invalid choice, please enter a valid choice")
+        elif choice == "1":
+            input_string = input("Please enter the string to be encrypted: ")
+            key = int(input("Please enter off-set: ").strip())
+
+            print(encrypt(input_string, key))
+        elif choice == "2":
+            input_string = input("Please enter the string to be decrypted: ")
+            key = int(input("Please enter off-set: ").strip())
+
+            print(decrypt(input_string, key))
+        elif choice == "3":
+            input_string = input("Please enter the string to be decrypted: ")
+            brute_force_data = brute_force(input_string)
+
+            for key, value in brute_force_data.items():
+                print(f"Key: {key} | Message: {value}")
+
+        elif choice == "4":
+            print("Goodbye.")
+            break
