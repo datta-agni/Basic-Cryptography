997ebada135e2f096d3207068633b966484a8392
datta-agni <dattadunga@gmail.com>
2021-08-18 13:31:21 +0530

    Reformatted and Updated the code with better readibilty and efficiency.

--- Asymmetric_Key_Cryptography/rsa.py
+++ Asymmetric_Key_Cryptography/rsa.py
@@ -1,44 +1,49 @@
 # basic implementation of Rivest–Shamir–Adleman algorithm
-"""
-Used for generating random numbers used for prime checking
-"""
+"""Used for generating random numbers used for prime checking"""
 import random
 
 """
 Returns a non-negative Python integer with k random bits. This method is supplied with the MersenneTwister Generator
 """
-maximum_prime_length = int(input("Enter the number of bits for generation: "))
+maximum_prime_length = int(
+    input(
+        """
+        Enter the number of bits for generation.
+        It should be above 128bits.
+        ENTER : """
+    )
+)
 
 
-# calculates the modular inverse from e and phi
-def euler_totient(a, b):
-    if a == 0:
-        return (b, 0, 1)
+def modular_inverse(number1, number2):
+    """calculates the modular inverse from e and phi"""
+    if number1 == 0:
+        return (number2, 0, 1)
     else:
-        g, y, x = euler_totient(b % a, a)
-        return (g, x - (b // a) * y, y)
+        g, y, x = modular_inverse(number2 % number1, number1)
+        return (g, x - (number2 // number1) * y, y)
 
 
-# calculates the gcd of two integers
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
+def gcd(number1, number2):
+    """calculates the gcd of two integers"""
+    while number2 != 0:
+        number1, number2 = number2, number1 % number2
+    return number1
 
 
-# check composite number for rabin-miller primality test
 def try_composite(a, d, n, s):
+    """check composite number for rabin-miller primality test"""
     if pow(a, d, n) == 1:
         return False
     for i in range(s):
         if pow(a, 2 ** i * d, n) == n - 1:
             return False
-    # n  is definitely composite
+    """n is definitely composite"""
     return True
 
 
-# checks if a number is a prime through miller-rabin primality test
 def is_prime(n, precision_for_huge_n=16):
+    """checks if a number is a prime through miller-rabin primality test"""
     if n in known_primes:
         return True
     if any((n % p) == 0 for p in known_primes) or n in (0, 1):
@@ -60,7 +65,7 @@ def is_prime(n, precision_for_huge_n=16):
         return not any(try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
     if n < 341550071728321:
         return not any(try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
-    # otherwise
+    """otherwise"""
     return not any(
         try_composite(a, d, n, s) for a in known_primes[:precision_for_huge_n]
     )
@@ -69,38 +74,42 @@ def is_prime(n, precision_for_huge_n=16):
 known_primes = [2, 3]
 known_primes += [x for x in range(5, 1000, 2) if is_prime(x)]
 
-# function used to generate random number which inturn are checked for being prime
+
 def generate_random_prime():
+    """
+    function used to generate random number which inturn are checked for being prime
+    """
     while 1:
-        ranPrime = random.getrandbits(maximum_prime_length)
-        if is_prime(ranPrime):
-            return ranPrime
+        random_prime = random.getrandbits(maximum_prime_length)
+        if is_prime(random_prime):
+            return random_prime
 
 
-# function used to generate the public-key, private-key pair
 def generate_key_pair():
+    """
+    function used to generate the public-key, private-key pair
+    """
+
+    """generates to random primes for 'p' and 'q'"""
     p = generate_random_prime()
     q = generate_random_prime()
-
     n = p * q
-    print("n:\n", n)
-    # phi(n) = phi(p)*phi(q)
-    phi = (p - 1) * (q - 1)
-    print("phi:\n", phi)
 
-    # choose e coprime to n and 1 > e > phi
+    """phi(n) = phi(p) * phi(q)"""
+    phi = (p - 1) * (q - 1)
+    print("n:\n", n, "\n" "phi:\n", phi, "\n")
+
+    """choose e coprime to n and 1 > e > phi"""
     e = random.randint(1, phi)
     g = gcd(e, phi)
     while g != 1:
         e = random.randint(1, phi)
         g = gcd(e, phi)
 
-    print("e:\n", e)
-    print("phi:\n", phi)
-    # d[1] = modular inverse of e and phi
-    d = euler_totient(e, phi)[1]
+    """d[1] = modular inverse of e and phi"""
+    d = modular_inverse(e, phi)[1]
 
-    # make sure d is positive
+    """make sure d is positive"""
     d = d % phi
     if d < 0:
         d += phi
@@ -108,8 +117,8 @@ def generate_key_pair():
     return ((e, n), (d, n))
 
 
-# function used for decryption
 def decrypt(ciphertext, private_key):
+    """function used for decryption"""
     try:
         key, n = private_key
         text = [chr(pow(char, key, n)) for char in ciphertext]
@@ -118,28 +127,39 @@ def decrypt(ciphertext, private_key):
         print(e)
 
 
-# function used for encryption
 def encrypt(text, public_key):
+    """function used for encryption"""
     key, n = public_key
     ciphertext = [pow(ord(char), key, n) for char in text]
     return ciphertext
 
 
-# main function
-def main():
-
+"""main function"""
+if __name__ == "__main__":
     public_key, private_key = generate_key_pair()
     text = str(input("Enter the text you want to be encrypted with RSA: "))
-
-    print("Public Key: ", public_key, "\n")
-    print("Private Key: ", private_key, "\n")
-
-    ciphertext = encrypt(text, public_key)
+    ciphertext = encrypt(str(text.encode("utf-8")), public_key)
     plaintext = decrypt(ciphertext, private_key)
 
-    print("Encrypted Text =", ciphertext, "\n")
-    print("Decrypted Text =", plaintext, "\n")
-
-
-if __name__ == "__main__":
-    main()
+    print(
+        "Public Key: "
+        + str(public_key)
+        + "\n"
+        + "\n"
+        + "Private Key: "
+        + "\n"
+        + "\n"
+        + str(private_key)
+        + "\n"
+        + "\n"
+        + "Encrypted Text: "
+        + "\n"
+        + "\n"
+        + str(ciphertext)
+        + "\n"
+        + "\n"
+        + "Decrypted Text: "
+        + "\n"
+        + "\n"
+        + str(plaintext)
+    )
